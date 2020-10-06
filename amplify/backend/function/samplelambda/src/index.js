4288be5963a9aaa4aa585843df28a085984f12bc
newslab <s03001-gotou.k@nhk.or.jp>
2020-10-06 16:55:26 +0900

    追加：ラムダを叩いてs3にjsonを保存する

--- amplify/backend/function/samplelambda/src/index.js
+++ amplify/backend/function/samplelambda/src/index.js
@@ -2,16 +2,29 @@
 	ENV
 	REGION
 Amplify Params - DO NOT EDIT */
+const AWS = require("aws-sdk");
+const S3 = new AWS.S3();
 
 exports.handler = async (event) => {
-    // TODO implement
+
+    
+    await S3.putObject({
+      Bucket: "dev-goto-test",
+      Key: `test.json`,
+      Body: JSON.stringify({ name: "kensukegoto" }),
+      ContentType: "application/json"
+    }).promise()
+
+    console.log("こんにちは");
+
+
     const response = {
         statusCode: 200,
     //  Uncomment below to enable CORS requests
      headers: {
          "Access-Control-Allow-Origin": "*"
      }, 
-        body: JSON.stringify('Hello from Lambda!'),
+        body: JSON.stringify('ごとうけんすけ!'),
     };
     return response;
 };
